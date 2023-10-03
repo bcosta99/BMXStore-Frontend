d89aba38d5dce4e774cd79043ec0a38385e940e7
Brenno <brennocostahh@hotmail.com>
2023-10-03 11:55:27 +0200

    Route localhost:3000 to element Home

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ export default function App() {
             <BrowserRouter>
                 <Navbar/>
                 <Routes>
+                    <Route path='' element={<Home></Home>}></Route>
                     <Route path='/home' element={<Home></Home>}></Route>
 
                     <Route path='/products' element={<ItemList></ItemList>}></Route>
